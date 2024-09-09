97fad72dbad34fe4adf595ce10be2acc38b933c0
Cengiz <ata.kaan.cengiz@outlook.de>
2024-09-09 21:03:57 +0200

    directory restucture

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,38 +4,53 @@ extends CharacterBody2D
 var SPEED = 200
 const JUMP_VELOCITY = -300
 var second_jump = true
+var timer_startet = false
 @onready var anim = $AnimationPlayer
 @onready var sprite_player: Sprite2D = %SpritePlayer
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var kill_timer: Timer = $KillTimer
 
+func animation_handler():
 
+	#IDLE ANIMATION
+	if is_on_floor:
+		anim.play("Idle")
 
+	#JUMP ANIMATION
+	if Input.is_action_just_pressed("Up") and is_on_floor():
+		anim.play("Jump")
+		
+	#SLASH ANIMATION
+	if Input.is_action_just_pressed("ClickLeft"):
+		animated_sprite_2d.visible = true
+		animated_sprite_2d.play()
 
-func _physics_process(delta: float) -> void:
+	#DIRECTION
+	var direction := Input.get_axis("Left", "Right")
+	if direction>0:
+		sprite_player.flip_h = false
+		animated_sprite_2d.flip_h = true
+	elif direction<0:
+		sprite_player.flip_h = true
+		animated_sprite_2d.flip_h = false
+
+func movement_handler(delta):
 	
-	# Add the gravity.
 	if is_on_floor():
 		second_jump = true
-		anim.play("Idle")
 	else:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		anim.play("Jump")
 	
 		
-
-	
 	if Input.is_action_just_pressed("Up") and not is_on_floor() and second_jump:
 		velocity.y = JUMP_VELOCITY
 		second_jump = false
 		
-	if Input.is_action_just_pressed("ClickLeft"):
-		animated_sprite_2d.visible = true
-		animated_sprite_2d.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -43,15 +58,29 @@ func _physics_process(delta: float) -> void:
 	
 	if direction>0:
 		velocity.x = direction * SPEED
-		sprite_player.flip_h = false
-		animated_sprite_2d.flip_h = true
 	elif direction<0:
-		sprite_player.flip_h = true
-		animated_sprite_2d.flip_h = false
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	
 
+func death_handler():
+	if not timer_startet and not is_on_floor():
+		kill_timer.start()
+		timer_startet = true
+	if is_on_floor():
+		kill_timer.stop()
+		timer_startet = false
+	
+	
+func _physics_process(delta: float) -> void:
+	death_handler()
+	
+	animation_handler()
+	
+	movement_handler(delta)
+	
 	move_and_slide()
+
+
+func _on_kill_timer_timeout() -> void:
+	get_tree().reload_current_scene()
